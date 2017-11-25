51f7e75a43c1400900e01ae82f564b002fe13849
N. Justus <nicola.justus@mni.thm.de>
2017-11-25 19:38:45 +0100

    Add buttons for switching the logic interpreter

--- src/cljs/lwb_ui/core.cljs
+++ src/cljs/lwb_ui/core.cljs
@@ -2,6 +2,10 @@
   (:require [cljs.nodejs :as node]
             [lwb-ui.atom :as atom]))
 
+;;TODO:
+;; - buttons for switching logic
+;; - natural deduction?
+
 ;; reference to atom shell API
 (def ashell (node/require "atom"))
 
@@ -45,21 +49,42 @@
              :prop "(ns prop (:require [lwb.prop :refer :all]))"
              :pred "(ns pred (:require [lwb.pred :refer :all]))"
              :ltl  "(ns ltl (:require [lwb.ltl :refer :all]))"
-  })
+             })
+
+;;matches any namespace of 'header' containing lwb.*
+(def ns-regex #"\(ns \w+ \(:require \[lwb\.\w+ :refer :all\]\)\)")
+
+(defn switch-namespace [namespace]
+  (let [editor (.getActiveTextEditor atom/workspace)]
+    (.scan editor ns-regex (fn [match] (.replace match namespace)))
+    (reset-repl)
+    ))
+
+(defn use-prop []
+  (.log js/console "Hello World from prop")
+  (switch-namespace (:prop header)))
+(defn use-pred []
+  (.log js/console "Hello World from pred")
+  (switch-namespace (:pred header)))
+(defn use-ltl []
+  (.log js/console "Hello World from ltl")
+  (switch-namespace (:ltl header)))
+
 
 (defn start-lwb-ui []
-  (.log js/console "repling")
   (reset! started? true)
   (.onDidConnect js/protoRepl
     (fn []
       (reset-repl)))
-      ;(.addInfo "lwb-repl ready" atom/notifications)))
+      ;(.addInfo "lwb-repl ready" atom/notifications))) ;FIXME: fix it
   (-> (.open atom/workspace)
     (.then (fn [e] (.toggle js/protoRepl (.getPath repl-project-root)) e))
     (.then (fn [editor]
-              (.setGrammar editor (.grammarForScopeName atom/grammars "source.clojure"))
-              (.insertText editor (str (:prop header)))
-              (.activatePreviousPane atom/workspace)))
+             (.setGrammar editor (.grammarForScopeName atom/grammars "source.clojure"))
+             (.insertText editor (str (:prop header)))
+             (.insertNewline editor)
+             (.insertNewline editor)
+             (.activatePreviousPane atom/workspace)))
     ))
 
 (defn stop-lwb-ui []
@@ -78,7 +103,11 @@
       (.then #(pck-commands))))
 
 (defn pck-commands []
-  (.add atom/commands "atom-workspace" "lwb-ui:toggle" toggle))
+  (.add atom/commands "atom-workspace" "lwb-ui:toggle" toggle)
+  (.add atom/commands "atom-workspace" "lwb-ui:prop" use-prop)
+  (.add atom/commands "atom-workspace" "lwb-ui:pred" use-pred)
+  (.add atom/commands "atom-workspace" "lwb-ui:ltl" use-ltl)
+)
 
 ;; live-reload
 ;; calls stop before hotswapping code
