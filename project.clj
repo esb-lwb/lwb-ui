dd9bffeadd81ccd49dab55db5805a00d85b2f229
N. Justus <nicola.justus@mni.thm.de>
2017-12-06 10:53:29 +0100

    Add nRepl middleware for cljs

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [org.clojure/core.async "0.2.374"]]
+                 [org.clojure/core.async "0.2.374"]
+                 [com.cemerick/piggieback "0.2.1"]]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :source-paths ["src/cljs"]
   :profiles {:dev {:source-paths ["src/dev"]
