939c4966ba89c5d7a69d37bcd6769e24c1e601dc
N. Justus <nicola.justus@mni.thm.de>
2017-12-28 17:21:53 +0100

    Better documentation

--- src/cljs/lwb_ui/core.cljs
+++ src/cljs/lwb_ui/core.cljs
@@ -4,7 +4,6 @@
             [clojure.string :as s]))
 
 ;;TODO:
-;; - fix generated uberjar to include sat solvers
 ;; - enable user to view the examples: print the code into a new buffer
 
 ;; reference to atom shell API
@@ -14,18 +13,25 @@
 (def composite-disposable (.-CompositeDisposable ashell))
 
 ;; Atom for holding all disposables objects
-(def disposables (atom []))
+(def disposables "All disposables of the plugin" (atom []))
 ;; are we started?
-(def started? (atom false))
+(def started? "Is the plugin enabled?" (atom false))
 
 ;; Initialise new composite-disposable so we can add stuff to it later
 (def subscriptions (new composite-disposable))
 (swap! disposables conj subscriptions)
 
-(def pck-root (new atom/directory (.resolvePackagePath atom/packages "lwb-ui")))
-(def repl-project-root (.getSubdirectory pck-root "lwb-proj"))
+(def pck-root
+  "The root directory path of this package."
+  (new atom/directory (.resolvePackagePath atom/packages "lwb-ui")))
+(def repl-project-root
+  "The directory path of the default leiningen project.
+  This project runs the user-provided clojure code"
+  (.getSubdirectory pck-root "lwb-proj"))
 
-(def header {
+(def header
+  "The headers for each namespace that includes a specific logic."
+  {
              :prop '(ns prop
                       (:require [lwb.prop :refer :all]
                                 [lwb.prop.cardinality :as lc]
@@ -48,21 +54,20 @@
                       (:require [lwb.nd.repl :refer :all]))
              })
 
-;;matches any namespace of 'header' containing lwb.*
-(def ns-regex #"\(ns \w+ (?:\(:require (?:\[lwb\.[\w\.]+[^\]]*\]\s*)+\)\s*)+\)")
+(def ns-regex
+  "Regex for matching any value of 'header'."
+  #"\(ns \w+ (?:\(:require (?:\[lwb\.[\w\.]+[^\]]*\]\s*)+\)\s*)+\)")
 
 
-(defn reset-repl
-  "Sets the grammar of the editor to clojure, clears the repl and searches the first lwb requirement and executes it"
-  [editor]
+(defn reset-repl  [editor]
+  "Sets the grammar of the editor to clojure, clears the repl and searches the first namespace containing lwb requirements and executes it."
   (atom/set-grammar editor)
   (.scan editor ns-regex (fn [match]
     (.clearRepl js/protoRepl)
     (.executeCode js/protoRepl match.matchText))))
 
-(defn add-header
-  "Adds the given namespace to the given editor"
-  [editor namespace]
+(defn add-header [editor namespace]
+  "Adds the given namespace to the given editor."
   (let [buffer (.getBuffer editor)]
     (.insert buffer 0 "\n\n")
     (.insert buffer 0 namespace)))
@@ -78,7 +83,8 @@
     (.open atom/workspace)))
 
 (defn switch-namespace
-  "Adds or replaces the given namespace to the given or active editor."
+  "Adds or replaces the given namespace to the given editor.
+  If no editor provided, use the current active one."
   ([namespace logic-name]
    (-> (get-editor)
        (.then (fn [editor]
@@ -103,26 +109,24 @@
          (add-header editor (str namespace))))
      (throw (js/Error. "Oops!")))))
 
-(defn use-prop
-  "Changes the namespace to \"Propositional Logic\""
-  []
+(defn use-prop []
+  "Changes the namespace (therefore logic) to 'Propositional Logic'."
   (switch-namespace (:prop header) "Propositional Logic"))
-(defn use-pred
-  "Changes the namespace to \"Predicate Logic\""
-  []
+(defn use-pred []
+  "Changes the namespace (therefore logic) to 'Predicate Logic'."
   (switch-namespace (:pred header) "Predicate Logic"))
-(defn use-ltl
-  "Changes the namespace to \"Linear Temporal Logic\""
-  []
+(defn use-ltl []
+  "Changes the namespace (therefore logic) to 'Linear Temporal Logic'."
   (switch-namespace (:ltl header) "Linear Temporal Logic"))
-(defn use-nd
-  "Changes the namespace to \"Natural Deduction\""
-  []
+(defn use-nd []
+  "Changes the namespace (therefore logic) to 'Natural Deduction'."
   (switch-namespace (:nd header) "Natural Deduction"))
 
-(defn start-lwb-ui
-  "Resets the repl if it's ready. Starts the repl with a default project. Last checks the editor, if it is empty the lwb header for \"Propositional Logic\" will be added."
-  []
+(defn start-lwb-ui []
+  "Starts the lwb-ui by doing the following:
+	- If proto-repl is ready, reset it
+	- Load the default project from 'repl-project-root' into proto-repl
+	- If the current editor does not contain an lwb header add the header for 'Propositional Logic'"
   (reset! started? true)
   (-> (get-editor)
     (.then (fn [editor]
@@ -153,18 +157,18 @@
   nil)
 
 (defn install-dependent-packages []
+  "Install all atom packages that are a dependency of this plugin."
   (.install (node/require "atom-package-deps") "lwb-ui"))
 
-(defn toggle
-  "Starts or stops the lwb-ui"
-  []
+(defn toggle []
+  "Starts or stops the lwb-ui."
     (if @started?
       (stop-lwb-ui)
       (start-lwb-ui)))
 
-(defn pck-commands
-  "register the commands at atom"
-  []
+(defn pck-commands []
+  "Register the commands at atom.
+  The commands are then available in the command palette."
   (.add atom/commands "atom-workspace" "lwb-ui:toggle" toggle)
   (.add atom/commands "atom-text-editor" "lwb-ui:propositonal-logic" use-prop)
   (.add atom/commands "atom-text-editor" "lwb-ui:predicate-logic" use-pred)
@@ -173,15 +177,13 @@
 )
 
 ;; Dispose all disposables
-(defn deactivate
-  "runs if the plugin is deactivated or uninstalled"
-  []
+(defn deactivate []
+  "Executed ones to deactivate this package."
     (doseq [disposable @disposables]
       (.dispose disposable)))
 
-(defn activate
-  "runs if the plugin is activated or installed"
-  [state]
+(defn activate [state]
+  "Executed ones to activate this package."
   (-> (install-dependent-packages)
       (.then #(pck-commands))))
 
